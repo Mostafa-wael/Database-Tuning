71060422bf095e9038a27e56b46b37d7182698e6
Mostafa-wael <mostafa.w.k000@gmail.com>
2022-12-13 23:01:11 +0200

    return psql to docker compose

--- sql/queries/query1.sql
+++ sql/queries/query1.sql
@@ -1,1 +1,2 @@
-SELECT * FROM users;
+-- select all the users with more than 100 posts and more than 100 comments
+select * from users where id in (select user_id from posts group by user_id having count(*) > 100) and id in (select user_id from comments group by user_id having count(*) > 100); 

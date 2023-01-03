ed33fcf37944bb82193bd8b0a68df0e3f186b838
Mostafa-wael <mostafa.w.k000@gmail.com>
2023-01-04 00:56:55 +0200

    add q4

--- sql/queries/query4.sql
+++ sql/queries/query4.sql
@@ -1,6 +1,10 @@
--- get users wuth most messages form theri followers
 
-
-
-
-
+SELECT up.post_id , p.body , p.title  
+FROM  users u
+INNER JOIN user_posts up ON u.id = up.user_id
+INNER JOIN user_comments uc ON u.id = uc.user_id
+INNER JOIN posts p ON p.id = up.post_id
+INNER JOIN comments c ON c.id = uc.comment_id
+WHERE u.age > 28 and u.City = 'Giza' and
+p.title LIKE '%title 12%' and p.body LIKE '%body 22%' 
+GROUP BY up.post_id, p.body, p.title;

3a15d54b32e8af21262671a40437aa22724a0217
Ahmed_Ihab <ahmed.ihab@tactful.ai>
2023-01-04 07:08:57 +0200

    Q2 nosql

--- NoSQL/dummyData/insert_dummy.js
+++ NoSQL/dummyData/insert_dummy.js
@@ -1,3 +1,27 @@
 // store start time
 var start = new Date().getTime();
 
+
+// initialize bulk operation
+var bulk = db.dummy.initializeUnorderedBulkOp();
+// get comment id and post id for each user age greater than 25 and lives in city 1
+// loop through each user
+db.users.find({age: {$gt: 25}, city: 'city 1'}).forEach(function(user) {
+    db.user_posts.find({user_id: user.id}).forEach(function(post) {
+        db.user_comments.find({post_id: post.post_id}).forEach(function(comment) {
+            bulk.insert({
+                user_id: user._id,
+                post_id: post._id,
+                comment_id: comment._id
+            });
+        });
+    });
+});
+// execute bulk operation
+bulk.execute();
+
+
+// store end time
+var end = new Date().getTime();
+// print the time taken in milliseconds
+print(end - start + " ms");
--- nosql/connect_db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -it mongodb /bin/bash

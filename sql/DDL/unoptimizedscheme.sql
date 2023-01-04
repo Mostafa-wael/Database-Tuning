81c31a83e9c72d47776a52757bcfc435c91392ee
osamamagdy <osamamagdy174@gmail.com>
2023-01-04 03:10:10 +0200

    update schema and fill data

--- sql/DDL/unoptimizedscheme.sql
+++ sql/DDL/unoptimizedscheme.sql
@@ -1,5 +1,6 @@
 -- CREATE DATABASE unoptimized_db;
 -- SELECT 'unoptimized_db';
+
 CREATE TABLE users (
   id SERIAL NOT NULL,
   first_name varchar(255) NOT NULL,
@@ -26,7 +27,7 @@ CREATE TABLE user_posts (
   user_id int NOT NULL,
   post_id int NOT NULL,
   PRIMARY KEY (id),
-
+  UNIQUE (user_id, post_id),
   CONSTRAINT user_posts_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT user_posts_ibfk_2 FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -46,10 +47,12 @@ CREATE TABLE user_comments (
   post_id int NOT NULL,
   comment_id int NOT NULL,
   PRIMARY KEY (id),
-
+  UNIQUE (user_id, post_id, comment_id),
   CONSTRAINT user_comments_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT user_comments_ibfk_2 FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT user_comments_ibfk_3 FOREIGN KEY (comment_id) REFERENCES comments (id) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT user_comments_ibfk_3 FOREIGN KEY (comment_id) REFERENCES comments (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT user_comments_ibfk_4 FOREIGN KEY (user_id, post_id) REFERENCES user_posts (user_id, post_id)
+
 );
 
 
@@ -59,8 +62,8 @@ CREATE TABLE replies (
   user_id int NOT NULL,
   comment_id int NOT NULL,
   body text NOT NULL,
-  PRIMARY KEY (id)
-,
+  PRIMARY KEY (id),
+  UNIQUE (user_id, comment_id),
   CONSTRAINT replies_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT replies_ibfk_2 FOREIGN KEY (comment_id) REFERENCES comments (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -74,10 +77,12 @@ CREATE TABLE likes (
   id SERIAL NOT NULL,
   user_id int NOT NULL,
   post_id int NOT NULL,
-  PRIMARY KEY (id)
-,
+  PRIMARY KEY (id),
+  UNIQUE (user_id, post_id),
   CONSTRAINT likes_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT likes_ibfk_2 FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT likes_ibfk_2 FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT likes_ibfk_3 FOREIGN KEY (user_id, post_id) REFERENCES user_posts (user_id, post_id)
+
 );
 
 -- CREATE INDEX user_id ON likes (user_id);
@@ -89,8 +94,8 @@ CREATE TABLE follows (
   id SERIAL NOT NULL,
   user_id int NOT NULL,
   follower_id int NOT NULL,
-  PRIMARY KEY (id)
-,
+  PRIMARY KEY (id),
+  UNIQUE (user_id, follower_id),
   CONSTRAINT follows_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT follows_ibfk_2 FOREIGN KEY (follower_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -104,8 +109,7 @@ CREATE TABLE messages (
   sender_id int NOT NULL,
   receiver_id int NOT NULL,
   body text NOT NULL,
-  PRIMARY KEY (id)
-,
+  PRIMARY KEY (id),
   CONSTRAINT messages_ibfk_1 FOREIGN KEY (sender_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT messages_ibfk_2 FOREIGN KEY (receiver_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
